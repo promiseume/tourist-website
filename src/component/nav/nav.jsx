e5fa48cf8a52a895b5c1ebcc4b73e592fd63ea91
Promise Ume <ngoziume1@gmail.com>
2021-09-27 23:17:07 +0100

    tourist site

---
+++ src/component/nav/nav.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './nav.css'
+export default function Nav() {
+    return (
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a id='navnav' class="navbar-brand" href="#">Karya</a>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search for destination" aria-label="Search"/>
+      </form>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li id='navnav-item'  class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Service</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Destinations</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  )
+}
