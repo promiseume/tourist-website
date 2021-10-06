d712aecca630ef1ad7be66bd670345cabfcc68e3
Promise Ume <ngoziume1@gmail.com>
2021-10-06 17:26:15 +0100

    added some new change

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -3,10 +3,7 @@ import './footer.css'
 export default function Footer() {
     return (
         <div className='footer'>
-            <div>
-                <h2>Terms of service</h2>
-            </div>
-            <div><h2>Contact us</h2></div>
+            
         </div>
     )
 }
